9c02760a9822929587798308c5f18ec183c0a796
Danilo LM <danilolopesdemoraes@gmail.com>
2020-07-03 10:56:29 -0300

    Passando formulario para dentro de um Modal

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,12 +1,65 @@
-import 'package:despesas_pessoais/components/transaction_user.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:despesas_pessoais/model/transaction.dart';
+import 'package:despesas_pessoais/components/transaction_form.dart';
+import 'package:despesas_pessoais/components/transaction_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Fralda P 20 unidades',
+      value: 19.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Nubulizador G-tech',
+      value: 210.00,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      },
+    );
+  }
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transaction.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add_box),
+            onPressed: () => _openTransactionFormModal(context),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -19,9 +72,13 @@ class HomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            TransactionUser(),
+            TransactionList(_transaction),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_box),
+        onPressed: () => _openTransactionFormModal(context),
       ),
       bottomSheet: BottomAppBar(
         color: Colors.white,
